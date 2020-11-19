79c126f96e6262072adfae7bf115a0614bb44ddc
anyue <1183767577@qq.com>
2020-11-19 09:40:22 +0800

    添加基本类型。

--- DarkMoonProject/Assets/Game/Scripts/Base/BaseCard.cs
+++ DarkMoonProject/Assets/Game/Scripts/Base/BaseCard.cs
@@ -3,9 +3,14 @@
  * @Date: 2020-11-18 16:44:09 
  * @Description: 卡牌基类
  */
+
+using System.Collections.Generic;
 using UnityEngine;
-public class BaseCard : MonoBehaviour {
+
+public abstract class BaseCard : MonoBehaviour {
 
     protected int energyConsume = 1;
+
+    protected List<int> abilityList = new List<int> ();
 
 }
