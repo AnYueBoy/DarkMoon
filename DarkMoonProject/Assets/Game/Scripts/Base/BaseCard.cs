eff85988db1e17fc1ed8fcae82d2cb05b521782c
anyue <1183767577@qq.com>
2020-11-30 21:26:59 +0800

    添加卡牌的基本方法。

--- DarkMoonProject/Assets/Game/Scripts/Base/BaseCard.cs
+++ DarkMoonProject/Assets/Game/Scripts/Base/BaseCard.cs
@@ -28,14 +28,54 @@ public abstract class BaseCard : MonoBehaviour {
     }
 
     protected void turnBegin () {
-
+        Dictionary<int, AbilityData> abilityDataDic = cardData.abilityDataDic;
+        if (abilityDataDic == null) {
+            Debug.LogWarning ("abilityDic not exist in turnBegin");
+            return;
+        }
+        Dictionary<int, BaseAbility> abilityDic = AbilityManager.getInstance ().abilityDic;
+        if (abilityDic == null) {
+            Debug.LogWarning ("abilityDic not exist in turnBegin");
+            return;
+        }
+        foreach (int id in cardData.abilityDataDic.Keys) {
+            BaseAbility targetAbility = abilityDic[id];
+            // FIXME: 感觉应该传入的是abilityData cardData不属于能力插槽操作的数据,纠结ing
+            targetAbility.turnBeginEffect (cardData);
+        }
     }
 
     protected void playerTrigger () {
-
+        Dictionary<int, AbilityData> abilityDataDic = cardData.abilityDataDic;
+        if (abilityDataDic == null) {
+            Debug.LogWarning ("abilityDic not exist in playerTrigger");
+            return;
+        }
+        Dictionary<int, BaseAbility> abilityDic = AbilityManager.getInstance ().abilityDic;
+        if (abilityDic == null) {
+            Debug.LogWarning ("abilityDic not exist in playerTrigger");
+            return;
+        }
+        foreach (int id in cardData.abilityDataDic.Keys) {
+            BaseAbility targetAbility = abilityDic[id];
+            targetAbility.effect (cardData);
+        }
     }
 
     protected void turnEnd () {
-
+        Dictionary<int, AbilityData> abilityDataDic = cardData.abilityDataDic;
+        if (abilityDataDic == null) {
+            Debug.LogWarning ("abilityDic not exist in turnEnd");
+            return;
+        }
+        Dictionary<int, BaseAbility> abilityDic = AbilityManager.getInstance ().abilityDic;
+        if (abilityDic == null) {
+            Debug.LogWarning ("abilityDic not exist in turnEnd");
+            return;
+        }
+        foreach (int id in cardData.abilityDataDic.Keys) {
+            BaseAbility targetAbility = abilityDic[id];
+            targetAbility.turnEndEffect (cardData);
+        }
     }
 }
