c45e77865d47ce117af82007d22d635e10fe68f0
anyue <1183767577@qq.com>
2020-12-04 08:00:29 +0800

    添加必要卡牌数据字段

--- DarkMoonProject/Assets/Game/Scripts/Data/CustomCardData/CustomCardData.cs
+++ DarkMoonProject/Assets/Game/Scripts/Data/CustomCardData/CustomCardData.cs
@@ -4,11 +4,15 @@
  * @Date: 2020-12-04 07:45:21 
  * @Description: 自定义卡牌数据(用来读取卡池数据)
  * @Last Modified by: l hy
- * @Last Modified time: 2020-12-04 07:56:21
+ * @Last Modified time: 2020-12-04 08:00:10
  */
 
 public class CustomCardData {
     public int id = 0;
+
+    public int consumeEnergy = 0;
+
+    public string textureUrl = "";
 
     public List<CustomAbilityData> customAbilityDataList = new List<CustomAbilityData> ();
 }
