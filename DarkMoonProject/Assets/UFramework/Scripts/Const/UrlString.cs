8541771573fb9ecbcb65e2093725909d2d8ac23e
anyue <1183767577@qq.com>
2020-12-07 14:19:36 +0800

    添加能力列表

--- DarkMoonProject/Assets/UFramework/Scripts/Const/UrlString.cs
+++ DarkMoonProject/Assets/UFramework/Scripts/Const/UrlString.cs
@@ -3,7 +3,7 @@
  * @Date: 2020-03-08 21:58:54 
  * @Description: 资源路径
  * @Last Modified by: l hy
- * @Last Modified time: 2020-12-07 13:54:18
+ * @Last Modified time: 2020-12-07 14:19:16
  */
 
 public class UrlString {
@@ -13,5 +13,5 @@ public class UrlString {
     /// </summary>
     public static readonly string uiUrl = "UI/";
 
-    public static readonly string cardJsonUrl = "CardPool";
+    public static readonly string cardJsonUrl = "Json/cardPool";
 }
