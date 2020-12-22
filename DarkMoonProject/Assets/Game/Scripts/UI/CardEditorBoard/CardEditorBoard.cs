18d688d61a3a2268af47aeb658183c1d73be0d88
anyue <1183767577@qq.com>
2020-12-22 19:36:26 +0800

    优化逻辑

--- DarkMoonProject/Assets/Game/Scripts/UI/CardEditorBoard/CardEditorBoard.cs
+++ DarkMoonProject/Assets/Game/Scripts/UI/CardEditorBoard/CardEditorBoard.cs
@@ -1,10 +1,10 @@
-﻿using System.IO;
-/*
+﻿/*
  * @Author: l hy 
  * @Date: 2020-12-07 14:31:33 
  * @Description: 卡牌编辑界面
  */
 
+using System.IO;
 using System.Collections.Generic;
 using LitJson;
 using UnityEngine;
